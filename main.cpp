9dca6a9b056e9b001f46cf0dcded305a6dc3672a
JesusKim <jesuskim666@gmail.com>
2019-03-11 13:16:35 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ int main()
 
 	float* b{};
 	new_init_s(b, 1.f); // Convert this line into comment to make assertion fail.
-	//delete_s(b);
+	delete_s(b);
 	//delete_s(b);
 
 	long long* c{};
 	new_init_s(c, 1234);
-	//delete_s(c);
+	delete_s(c);
 
 	return 0;
 }
